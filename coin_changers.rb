6a4a637b002a7688265d939f8a6383780d822d86
TONYA JENNINGS <you@example.com>
2017-07-18 00:14:53 -0400

    coin_changers copy

--- coin_changers.rb
+++ coin_changers.rb
@@ -1,9 +1,27 @@
-def get_change(cents_received)
-	change = {}
-	if cents_received == 1
-		change['penny'] = 1
+def get_change(change_value)
+	change_hash = {}
+	# amount_remaining = change_value
+	if change_value >= 100 
+		change_hash["dollar"] = change_value / 100
+		change_value = change_value % 100
+	end
+	if change_value >= 25 
+		change_hash["quarter"] = change_value / 25
+		change_value = change_value % 25
+	end
+	if change_value >= 10 
+		change_hash["dime"] = change_value / 10
+		change_value = change_value % 10
+	end 
+	if change_value >= 5
+		change_hash["nickel"] = 1
+		change_value = change_value - 5
+	end
+	if change_value >= 1
+		change_hash["penny"] = change_value
+	end
+	change_hash
 end
 
-	change
 
-end
+#This is a copy of Trixie's files.  Anita and I could not get this to work even after many tries.
--- coin_changers_test.rb
+++ coin_changers_test.rb
@@ -3,18 +3,43 @@ require_relative "coin_changers.rb"
 
 class TestCoinChangers < Minitest::Test
 
-	def test_assert_that_1_equals_1
-		assert_equal(1, 1)
-	end
+	require "minitest/autorun"
 
-	def test_assert_that_zero_cents_returns_empty
-		assert_equal({},get_change,(0))
-	end
-
-	def test_assert_that_one_penny_returns_1_cent
-		assert_equal({"penny" => 1}, get_change(1))
-	end
-
-
-
+def test_assert_that_1_equals_1
+	assert_equal(1,1)
 end
+
+def test_assert_that_zero_cents_equals_empty_hash
+	assert_equal({}, get_change(0))
+end
+
+def test_assert_that_one_cent_equals_penny
+	assert_equal({"penny" => 1}, get_change(1))
+end
+
+def test_assert_that_two_cents_equals_two_pennies
+	assert_equal({"penny" => 2}, get_change(2))
+end
+
+def test_assert_that_five_cents_equals_nickel
+	assert_equal({"nickel" => 1}, get_change(5))
+end
+
+def test_assert_that_seven_cents__equals_1_nickel_2_penny
+	assert_equal({"nickel" => 1, "penny" =>2}, get_change(7))
+end
+
+def test_assert_that_23_cents_equals_2_dime_3_penny
+	assert_equal({"dime" => 2, "penny" => 3}, get_change(23))
+end
+
+def test_assert_that_87_cents_equals_3_quarter_1_dime_2_penny
+	assert_equal({"quarter" => 3, "dime" => 1, "penny" => 2}, get_change(87))
+end
+
+def test_assert_that_223_cents_equals_2_dime_3_penny
+	assert_equal({"dollar" => 2, "dime" => 2, "penny" => 3}, get_change(223))
+end
+end
+
+#This is a copy of Trixie's file.  Anita and I could not get this to work even after many tries.
